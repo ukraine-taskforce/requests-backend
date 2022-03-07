b10016b2a23fad9695d292c09bcaffbcc4adf898
kostiantyntsykulenko <kostiantyn.tsykulenko@bolt.eu>
2022-03-07 20:10:40 +0200

    Real cities.

--- lambda/locations/locations.js
+++ lambda/locations/locations.js
@@ -1,5 +1,30 @@
 module.exports.handler = async (event) => {
   console.log('Request: ', event);
+  let cityData = require('./cities.json');
+
+  let locale;
+  if (event.queryStringParameters)
+    locale = event.queryStringParameters.locale;
+
+  if (!locale) locale = 'uk';
+
+  let result;
+  if (locale == 'uk')
+    result = cityData.map(function(c) {
+        var o = {};
+        o["id"] = c.en;
+        o["name"] = c.uk;
+
+        return o;
+    });
+  else
+    result = cityData.map(function(c) {
+        var o = {};
+        o["id"] = c.en;
+        o["name"] = c.en;
+
+        return o;
+    });
 
   return {
     statusCode: 200,
@@ -8,16 +33,7 @@ module.exports.handler = async (event) => {
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      locations: [
-        {
-          id: 1,
-          name: "Харків"
-        },
-        {
-          id: 2,
-          name: "Cуми"
-        }
-      ]
+      locations: result
     }),
   }
 }
