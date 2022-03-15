2c325a93e45e402ea76b1b5e738d03102119da6e
Konstantin Tsykulenko <kostiantyn.tsykulenko@bolt.eu>
2022-03-15 23:10:39 +0200

    New categories. (#16)

--- lambda/supplies/supplies.js
+++ lambda/supplies/supplies.js
@@ -1,3 +1,5 @@
+const supplyData = require('./supplies.json');
+
 module.exports.handler = async (event) => {
   console.log('Request: ', event);
 
@@ -9,7 +11,7 @@ module.exports.handler = async (event) => {
 
   let result;
   if (locale == 'uk')
-    result = getSupplies().map(function(s) {
+    result = supplyData.map(function(s) {
         var o = {};
         o["id"] = s.id;
         o["name"] = s.uk;
@@ -17,7 +19,7 @@ module.exports.handler = async (event) => {
         return o;
     });
   else
-    result = getSupplies().map(function(s) {
+    result = supplyData.map(function(s) {
         var o = {};
         o["id"] = s.id;
         o["name"] = s.en;
@@ -34,105 +36,5 @@ module.exports.handler = async (event) => {
     body: JSON.stringify({
       supplies: result
     }),
-  }
-
-  function getSupplies() {
-    return [
-        {
-            id: "food",
-            uk: "Продукти харчування",
-            en: "Food"
-        },
-        {
-            id: "water",
-            uk: "Вода",
-            en: "Water"
-        },
-        {
-            id: "baby_food",
-            uk: "Дитяче харчування",
-            en: "Baby Food"
-        },
-        {
-            id: "baby_products",
-            uk: "Дитячі товари",
-            en: "Baby Products"
-        },
-        {
-            id: "medical_kits_supplies",
-            uk: "Медичні набори / товари",
-            en: "Medical Kits / Supplies"
-        },
-        {
-            id: "personal_hygiene_kits",
-            uk: "Товари особистої гігієни",
-            en: "Personal hygiene kits"
-        },
-        {
-            id: "masks",
-            uk: "Маски",
-            en: "Masks"
-        },
-        {
-            id: "sanitary_pads",
-            uk: "Гігієнічні прокладки",
-            en: "Sanitary pads"
-        },
-        {
-            id: "tampons",
-            uk: "тампони",
-            en: "Tampons"
-        },
-        {
-            id: "torches",
-            uk: "Смолоскипи",
-            en: "Torches"
-        },
-        {
-            id: "batteries",
-            uk: "Батарейки",
-            en: "Batteries"
-        },
-        {
-            id: "candles",
-            uk: "Свічки",
-            en: "Candles"
-        },
-        {
-            id: "firestarter",
-            uk: "запальнички",
-            en: "Firestarter"
-        },
-        {
-            id: "bedding",
-            uk: "Постільна білизна",
-            en: "Bedding"
-        },
-        {
-            id: "sleeping_bags",
-            uk: "спальні мішки",
-            en: "Sleeping Bags"
-        },
-        {
-            id: "thermal_clothing",
-            uk: "Термоодяг",
-            en: "Thermal clothing"
-        },
-        {
-            id: "shoes",
-            uk: "Взуття",
-            en: "Shoes"
-        },
-        {
-            id: "clothes",
-            uk: "Одяг",
-            en: "Clothes"
-        },
-        {
-            id: "gloves",
-            uk: "Рукавички",
-            en: "Gloves"
-        }
-      ]
   }
 }
