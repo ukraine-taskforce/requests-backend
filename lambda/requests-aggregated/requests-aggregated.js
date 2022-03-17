4694841406854f31958d5ee1380b17457f78d034
Theo Dammaretz <theo@dammaretz.fr>
2022-03-17 11:07:26 +0100

    feat: add jwt check to aggregated

--- lambda/requests-aggregated/requests-aggregated.js
+++ lambda/requests-aggregated/requests-aggregated.js
@@ -1,11 +1,29 @@
+const { CognitoJwtVerifier } = require("aws-jwt-verify");
+const AWS = require("aws-sdk");
+
+// Verifier that expects valid access tokens:
+const verifier = CognitoJwtVerifier.create({
+  userPoolId: process.env.poolId,
+  tokenUse: "access",
+  clientId: process.env.clientId,
+});
+
 module.exports.handler = async (event) => {
-  const AWS = require('aws-sdk')
-  const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+  try {
+    await verifier.verify(event.headers["Authorization"]);
+  } catch (error) {
+    return {
+      statusCode: 401,
+      body: "Not Authorized",
+    };
+  }
+
+  const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
   const bucket = process.env.bucket;
 
   var params = {
     Bucket: bucket,
-    Prefix: 'requests-aggregated/part-'
+    Prefix: "requests-aggregated/part-",
   };
 
   const s3Objects = await s3.listObjectsV2(params).promise();
@@ -13,18 +31,21 @@ module.exports.handler = async (event) => {
 
   params = {
     Bucket: bucket,
-    Key: s3ObjectKey
+    Key: s3ObjectKey,
   };
 
   const s3Object = await s3.getObject(params).promise();
-  const data = s3Object.Body.toString('utf-8').split(/\r?\n/).filter(e => e != '').map(e => JSON.parse(e));
+  const data = s3Object.Body.toString("utf-8")
+    .split(/\r?\n/)
+    .filter((e) => e != "")
+    .map((e) => JSON.parse(e));
 
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify({ data: data }),
-  }
-}
+  };
+};
