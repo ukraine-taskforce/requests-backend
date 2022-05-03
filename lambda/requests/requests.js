3d37a3f367e3127cee743819443fef7fd5fcfd96
Konstantin Tsykulenko <kostiantyn.tsykulenko@bolt.eu>
2022-05-03 21:25:23 +0300

    Requests v2 implementation. (#20)

--- lambda/requests/requests.js
+++ lambda/requests/requests.js
@@ -1,33 +1,54 @@
 const AWS = require("aws-sdk");
 
+function response(statusCode) {
+    return {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({}),
+    };
+}
+
 module.exports.handler = async (event) => {
-  console.log("Request: ", event);
-  const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
-  let sqsUrl = process.env.sqs_url;
+    console.log("Request: ", event);
+    const sqs = new AWS.SQS({apiVersion: "2012-11-05"});
+    let sqsUrl = process.env.sqs_url;
 
-  let body = JSON.parse(event.body);
+    let body = JSON.parse(event.body);
 
-  let request = {
-    location: body.location,
-    supplies: body.supplies,
-    people: body.people,
-  };
+    let version;
+    if (event.pathParameters) version = event.pathParameters.version;
 
-  console.log("Send: ", request);
+    let request
+    if (version === "v2") {
+        request = {
+            version: "v2",
+            location: body.location,
+            supplies: body.supplies,
+            comments: body.comments,
+            name: body.name,
+            phoneNumber: body.phoneNumber,
+        };
+    } else if (version === "v1") {
+        request = {
+            location: body.location,
+            supplies: body.supplies,
+            people: body.people,
+        };
+    } else {
+        return response(404);
+    }
 
-  let payload = {
-    MessageBody: JSON.stringify(request),
-    QueueUrl: sqsUrl,
-  };
+    console.log("Send: ", request);
 
-  await sqs.sendMessage(payload).promise();
+    let payload = {
+        MessageBody: JSON.stringify(request),
+        QueueUrl: sqsUrl,
+    };
 
-  return {
-    statusCode: 201,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: JSON.stringify({}),
-  };
+    await sqs.sendMessage(payload).promise();
+
+    return response(201);
 };
