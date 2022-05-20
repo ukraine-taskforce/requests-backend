9dff5aeefac1928ebd5eeea17f07ec243b3b9d4e
AntonRaichuk <41113411+AntonRaichuk@users.noreply.github.com>
2022-05-20 10:42:31 +0200

    Revert "Add send sms endpoint"

--- lambda/send-sms/sns-client.js
+++
@@ -1,9 +0,0 @@
-const {SNSClient} = require("@aws-sdk/client-sns");
-
-let snsClient = null;
-
-exports.getSnsClient = async () => {
-    if (snsClient) return snsClient;
-    snsClient = new SNSClient({})
-    return snsClient;
-};
